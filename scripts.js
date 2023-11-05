fe102b9bf94a38303f9a74895b32e9e39b1690ab
kraemlyn <cjdiboss21@gmail.com>
2023-11-04 20:05:12 -0400

    Edited files

--- scripts.js
+++ scripts.js
@@ -1,1 +1,18 @@
-/* Add your JavaScript to this file */
+document.addEventListener("DOMContentLoaded", function() {
+    const form = document.querySelector("form");
+    const userEmail = document.querySelector("#email");
+    const messageBox = document.querySelector(".message");
+
+    form.addEventListener("submit", function(event) {
+        event.preventDefault(); 
+
+        const Emailvalue = userEmail.value.trim();
+
+        if (Emailvalue === "") {
+            messageBox.textContent = "Please enter a valid email address.";
+        } else {
+            messageBox.textContent = `Thank you! Your email address ${Emailvalue} has been added to our mailing list!`;
+        }
+
+    });
+});
